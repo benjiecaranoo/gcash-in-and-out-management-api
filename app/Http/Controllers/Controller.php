48ef672a0b695d8469e8f8aface1d50d0c609e47
Benjie Caranoo <caranoobenjie@gmail.com>
2024-11-16 14:04:06 +0800

    add macro service to configure per page or request

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Support\ReturnResponse;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller
 {
-    use ReturnResponse;
+    use AuthorizesRequests, ReturnResponse;
 }
--- app/Http/Controllers/V1/Transaction/TransactionController.php
+++ app/Http/Controllers/V1/Transaction/TransactionController.php
@@ -10,23 +10,21 @@ use App\Http\Resources\TransactionResource;
 use App\Models\User;
 use Illuminate\Container\Attributes\CurrentUser;
 use Spatie\QueryBuilder\QueryBuilder;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    use AuthorizesRequests;
-
     /**
      * Display a listing of the resource.
      */
-    public function index(#[CurrentUser()] User $user)
+    public function index(Request $request, #[CurrentUser()] User $user)
     {
         /** TODO: add specific user transaction get data */
         $transactions = QueryBuilder::for(Transaction::class)
             ->where('user_id', $user->id)
             ->allowedFilters(['type', 'status'])
             ->allowedSorts(['created_at', 'amount'])
-            ->paginate();
+            ->simplePaginate($request->perPage());
 
         return TransactionResource::collection($transactions);
     }
